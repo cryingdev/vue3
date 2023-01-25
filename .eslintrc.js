2d0d3334074beb36aeb56a0634436fa3c9175c1f
cryingdev <cryingdev@ubivelox.com>
2023-01-25 09:48:36 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,16 @@ module.export = {
         browser: true,
         node: true
     },
-    extends: [],
-    parserOption: [],
-    rules: {}
+    extends: [
+        //vue
+        'plugin:vue/vue3-strongly-recommended',
+        //js
+        'eslint:recommended'
+    ],
+    parserOption: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+
+    }
 }
