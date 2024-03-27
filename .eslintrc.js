3c9e82b5bda5664dd7ad01f1f868e902b9f7a5f2
cryingdev <33308581+cryingdev@users.noreply.github.com>
2024-03-27 11:11:03 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,18 @@ module.export = {
         parser: 'babel-eslint'
     },
     rules: {
-
+        "vue/html-closing-braket-newline": ["error", {
+            "singleline": "never",
+            "multiline": "never"
+        }],
+        "vue/html-self-closing": ["error", {
+            "html": {
+                "void": "always",
+                "normal": "never",
+                "component": "always"
+            },
+            "svg": "always",
+            "math": "always"
+        }]
     }
 }
